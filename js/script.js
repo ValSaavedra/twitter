8e87d850d243c07dd0831552de772afb661b3013
ValSaavedra <val.dicion@gmail.com>
2017-05-16 14:11:44 -0400

    avance

--- js/script.js
+++ js/script.js
@@ -3,8 +3,34 @@ function agregar(){
 }
 
 
+var tareas = document.getElementById("tarea").value;
+document.getElementById("tarea").value=""; //recupera la tarea y limpia textarea
 
+var cont = document.getElementById("contenedor"); //guarda las tareas
 
+var nuevasTareas = document.createElement("div");
+var textoNuevaTarea = document.createTextNode(tareas);
+var elementoContenedor = document.createElement("span");
+//nuevos nodos elementos
+
+elementoContenedor.appendChild(textoNuevaTarea);
+nuevasTareas.appendChild(elementoContenedor);
+cont.appendChild(nuevasTareas);
+//asigna padres a los nodos
+
+//crea checkbox e iconos y les da atributo y clase
+var chck = document.createElement("input");
+chck.type = "checkbox";
+chck.setAttribute("class","check");
+var basura = document.createElement("span");
+basura.classList.add("fa","fa-trash-o");
+var cora = document.createElement("span");
+cora.classList.add("fa", "fa-heart");
+
+nuevasTareas.appendChild(chck);
+nuevasTareas.appendChild(basura);
+nuevasTareas.appendChild(cora);
+//asigna padres a nodos
 
 
 
